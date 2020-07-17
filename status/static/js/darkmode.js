a8b51e35b9a25e3d8e4d12282b6d9bc8e10ee7c9
Manny Castaneda <Ecast221@gmail.com>
2020-07-17 14:22:10 -0400

    Fixed ticket details page, subscription page, table headers dark mode colors reflect accurately.  Fixed font color issue on tickets.

--- status/static/js/darkmode.js
+++ status/static/js/darkmode.js
@@ -39,7 +39,11 @@ let cssToggle = () => {
             }
     } else if (currentUrl.includes("/subscription/")) {
         var subscriptions = document.getElementById("subscribe");
+        var updateBox = document.getElementById("update");
+        update.classList.toggle("darkmode");
+        update.classList.toggle("whitefont");
         subscriptions.classList.toggle("eventDarkMode");
+
     } else if (currentUrl.includes("/details/")){
         var tickets = document.getElementById("ticket_info");
         tickets.classList.toggle("eventDarkMode");
